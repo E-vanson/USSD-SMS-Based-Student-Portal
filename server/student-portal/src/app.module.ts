d0dd2f967079c77fd1e1d34663437f501547d486
Evans <evansonkamau233@gmail.com>
2025-01-31 12:23:38 +0300

    Modules Initialized

--- server/student-portal/src/app.module.ts
+++ server/student-portal/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './student/admin/admin.module';
+import { AdminModule } from './admin/admin.module';
+import { StudentModule } from './student/student.module';
+import { CourseModule } from './course/course.module';
+import { UnitModule } from './unit/unit.module';
+import { PaymentModule } from './payment/payment.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, StudentModule, CourseModule, UnitModule, PaymentModule, NotificationsModule, ResultsModule],
   controllers: [AppController],
   providers: [AppService],
 })
