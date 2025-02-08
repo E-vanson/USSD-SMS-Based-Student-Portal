4412c94329da5b1fe59c433f5406479e271bf14d
Evans <evansonkamau233@gmail.com>
2025-02-08 13:13:13 +0300

    Initialized the controller

--- server/student-portal/src/unit/unit.controller.ts
+++ server/student-portal/src/unit/unit.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put, Get, Delete, Body, Param } from '@nestjs/common';
 
-@Controller('unit')
-export class UnitController {}
+import { UnitService } from './unit.service';
+import { CreateUnitDto } from './dto/create-unit.dto';
+import { UpdateUnitDto } from './dto/update-unit.dto';
+
+@Controller({path: 'unit', version: '1'})
+export class UnitController {
+    constructor(private unitsService: UnitService) { }
+    
+    @Get()
+    getUnits() {
+        return this.unitsService.getUnits();
+    }
+
+    @Get('/:id')
+    getAdmin(@Param('id') id: string) {
+        return this.unitsService.getUnit(id);
+    }
+
+    @Post()
+    createUnit(@Body() body: CreateUnitDto) {
+        return this.unitsService.createUnit(body);
+    }
+
+    @Put('/:id')
+    updateUnit(@Param('id') id: string, @Body() body: UpdateUnitDto) {
+        return this.unitsService.updateUnit(id, body);
+    }
+
+    @Delete('/:id')
+    deleteUnit(@Param('id') id: string) {
+        return this.unitsService.deleteUnit(id);
+    }
+
+}
