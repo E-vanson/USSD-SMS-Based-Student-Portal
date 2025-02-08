d0dc4e415e860c38eccc04afd2a685a462f78ac0
Evans <evansonkamau233@gmail.com>
2025-02-08 11:10:27 +0300

    Auth Module Initialized

--- server/student-portal/src/app.module.ts
+++ server/student-portal/src/app.module.ts
@@ -11,12 +11,13 @@ import { UnitModule } from './unit/unit.module';
 import { PaymentModule } from './payment/payment.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { ResultsModule } from './results/results.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }), ///for global env configuration
     MongooseModule.forRoot(process.env.DATABASE_URI!, { dbName: process.env.DATABASE_NAME,}), 
-    AdminModule, StudentModule, CourseModule, UnitModule, PaymentModule, NotificationsModule, ResultsModule],
+    AdminModule, StudentModule, CourseModule, UnitModule, PaymentModule, NotificationsModule, ResultsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService], 
 })
