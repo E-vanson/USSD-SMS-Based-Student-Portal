33c7e9bf3a7bda64003e5d04e616ff4980f34430
Evans <evansonkamau233@gmail.com>
2025-02-10 08:28:00 +0300

    Initialized the courses controller

--- server/student-portal/src/student/student.controller.ts
+++ server/student-portal/src/student/student.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put, Get, Delete, Body, Param } from '@nestjs/common';
 
-@Controller('student')
-export class StudentController {}
+import { CourseService } from 'src/course/course.service';
+import { CreateCourseDto } from 'src/course/dto/create-course.dto';
+import { UpdateCourseDto } from 'src/course/dto/update-course.dto';
+
+@Controller({path: 'student', version: '1'})
+export class StudentController {
+    constructor(private courseService: CourseService){}
+
+    @Get()
+    getCourses() {
+        return this.courseService.getCourses();
+    }
+
+    @Get('/:id')
+    getCourse(@Param('id') id: string) {
+        return this.courseService.getCourse(id)
+    }
+
+    @Post()
+    createCourse(@Body() body: CreateCourseDto) {
+        return this.courseService.createCourse(body);
+    }
+
+    @Put('/:id')
+    updateCourse(@Param('id') id: string, @Body() body: UpdateCourseDto) {
+        return this.courseService.updateCourse(id, body);
+    }
+
+    @Delete('/:id')
+    deleteCourse(@Param('id') id: string) {
+        return this.courseService.deleteCourse(id)
+    }
+}
