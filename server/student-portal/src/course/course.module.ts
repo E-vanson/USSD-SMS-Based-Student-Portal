6e004a90d450220423a9adfbd668806735723813
Evans <evansonkamau233@gmail.com>
2025-02-10 08:34:55 +0300

    Updated courses controller

--- server/student-portal/src/course/course.module.ts
+++ server/student-portal/src/course/course.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CourseController } from './course.controller';
 import { CourseService } from './course.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Courses, CoursesSchema } from './schema/course.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: Courses.name, schema: CoursesSchema}])],
   controllers: [CourseController],
   providers: [CourseService]
 })
