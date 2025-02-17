ea023b88f2bfbfb150d92cc410844843dde7b389
Evans <evansonkamau233@gmail.com>
2025-02-18 01:22:09 +0300

    Added state for results,fees & updates

--- server/student-portal/src/ussd/ussd.service.ts
+++ server/student-portal/src/ussd/ussd.service.ts
@@ -67,8 +67,69 @@ export class UssdService {
             defaultNext: "invalidOption",
         })
 
-        
-        
+        this.menu.state("Get Examination Results", {
+            run: () => {
+                this.menu.con(
+                    "Select Year and semester: " +
+                    "\n1. 1.1 " +
+                    "\n2. 1.2 " +
+                    "\n3. 2.1 " +
+                    "\n4. 2.2 " +
+                    "\n5. 3.1 " +
+                    "\n6. 3.2 "
+                )
+            },
+            next: {
+                1: "1.1",
+                2: "1.2",
+                3: "2.1",
+                4: "2.2",
+                5: "3.1",
+                6: "3.2"                
+            },
+            defaultNext: "Invalid Option"
+        })
+
+        this.menu.state("1.1", {
+            run: () => {                
+                this.menu.end("Your request is being processed kindly wait for a sms message");                
+            },            
+        });        
+
+        this.menu.state("Get Latest Updates", {
+            run: () => {
+                this.menu.con(
+                    "Choose Update Type: " +
+                    "\n1. Academic " +
+                    "\n2. Financial " +
+                    "\n3. General"
+                )                
+            },
+            next: {
+                1: "Academic",
+                2: "Financial ", 
+                3: "General"
+            },
+            defaultNext: "Invalid Option"
+        })
+
+        this.menu.state("Academic", {
+            run: () => {                
+                this.menu.end("Your request is being processed kindly wait for a sms message");                
+            },            
+        });     
+
+        this.menu.state("Financial", {
+            run: () => {                
+                this.menu.end("Your request is being processed kindly wait for a sms message");                
+            },            
+        });     
+
+        this.menu.state("General", {
+            run: () => {                
+                this.menu.end("Your request is being processed kindly wait for a sms message");                
+            },            
+        });     
   }
     
     
